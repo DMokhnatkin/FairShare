0a560bcc8c9ce99a190ffc137a418a775cca1660
Dmitriy Mokhnatkin <dmitriy.mokhnatkin@gmail.com>
2018-05-22 22:56:21 +0300

    Added android project

--- Client/FairShare.Client.Android/Properties/AssemblyInfo.cs
+++ Client/FairShare.Client.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FairShare.Android")]
+[assembly: AssemblyTitle("FairShare.Client.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FairShare.Android")]
+[assembly: AssemblyProduct("FairShare.Client.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
